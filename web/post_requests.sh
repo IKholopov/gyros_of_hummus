ff627be76f506431836c6b3cad19aca266b5b34b
IKholopov <kholopov96@gmail.com>
2016-11-27 12:35:40 +0300

    Coordinates changed

--- web/post_requests.sh
+++ web/post_requests.sh
@@ -3,7 +3,7 @@ SRV=localhost
 
 while true
 do
-	curl "http://$SRV/navigate/?floor_from=0&x_from=15.417994&y_from=60.483762&floor_to=1&x_to=15.418110&y_to=60.485317&user_id=1"
+	curl "http://$SRV/navigate/?floor_from=0&x_from=15.417994&y_from=60.483762&floor_to=1&x_to=15.418110&y_to=60.484717&user_id=1"
 	curl http://$SRV/create_route/ --data "user_id=1"
 	sleep 5
 	curl "http://$SRV/navigate/?floor_from=1&x_from=15.418270&y_from=60.484330&floor_to=0&x_to=15.418503&y_to=60.484684&user_id=3"
